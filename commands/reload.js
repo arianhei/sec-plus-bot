7d24ef683c2e6de5245ac7e78e0ace1823be7353
niko_kokawa <96712618+arianhei@users.noreply.github.com>
2022-02-28 09:06:20 +0330

    Reload bot commands

--- commands/reload.js
+++ commands/reload.js
@@ -1,4 +1,3 @@
-try{
 module.exports = {
     name: 'reload',
     execute(message)
@@ -7,30 +6,27 @@ module.exports = {
         const PREFIX = db.data(message.guild.id)
         const args = message.content.slice(PREFIX).trim().split(/ +/);
         delete db;
-        if(message.author.id !== '462847922330140692' && message.author.id !== '553139953597677568')
+        if(message.author.id !== 'OWNER_DISCORD_ID_HETE')
         {
-            return message.channel.send({embed: {fields: [{name: 'Error 0x403: this command isnt avaible for other peoples <a:qal:777127378253774858>',value: 'only DEV\'s',}]}})
+            return message.channel.send({embed: {fields: [{name: 'Error 0x403: this command isnt avaible for other people',value: 'only DEV\'s',}]}})
         }
         if(args.length === 1)
         {
-            return message.channel.send({embed: {title: "please enter which command to reload <a:qal:777127378253774858>"}})
+            return message.channel.send({embed: {title: "please enter which command to reload"}})
         }
-        if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}! <a:qal:777127378253774858>`);
+        if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
         const commandName = args[1].toLowerCase();
         const command = message.client.commands.get(commandName)
 
-        if (!command) return message.channel.send(`There is no command with name \`${commandName}\`, ${message.author}! <a:qal:777127378253774858>`);
+        if (!command) return message.channel.send(`There is no command with name \`${commandName}\`, ${message.author}!`);
         delete require.cache[require.resolve(`./${command.name}.js`)];
         try {
             const newCommand = require(`./${command.name}`);
             message.client.commands.set(newCommand.name, newCommand);
-            message.channel.send('reloaded successfully <a:tami:777128773480742932>')
+            message.channel.send('reloaded successfully')
         } catch (error) {
             console.error(error);
-            message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\` <a:qal:777127378253774858>`);
+            message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
         }
     }
-}
-}catch(err){
-    console.log(err)
 }
