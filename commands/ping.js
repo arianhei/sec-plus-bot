7c442f44275bf62abf47e4fbdd91d1e9690d03bb
niko_kokawa <96712618+arianhei@users.noreply.github.com>
2022-02-28 09:04:53 +0330

    Network Latency(Ping)

--- commands/ping.js
+++ commands/ping.js
@@ -1,15 +1,15 @@
 module.exports = {
     name: "ping",
-    description : "get bot ping legacy <a:tik3:777128127804735518>",
+    description : "get bot ping legacy",
     execute(message) {
-            const ping = require('jjg-ping')
+            const ping = require('jjg-ping');
             ping.system.ping('discord.com', function (latency, status) {
                 if (status) {
-                    // Host is reachable/up. Latency should have a value.
+                    // Host is reachable/up. Latency must have a value.
                     message.channel.send({ embed: { title: 'Ping (' + latency + ' ms ping).', color: "GREEN" } });
                 }
                 else {
-                    // Host is down. Latency should be 0.
+                    // Host is down.
                     console.log({ embed: { title: 'Ping is unreachable.' } });
                 }
 
