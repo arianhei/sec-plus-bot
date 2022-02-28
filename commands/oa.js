43122736233186afdbb41f9785243b91ed0d2b91
niko_kokawa <96712618+arianhei@users.noreply.github.com>
2022-02-28 09:03:04 +0330

    Open all channels

--- commands/oa.js
+++ commands/oa.js
@@ -1,23 +1,19 @@
-try{
 module.exports = {
     name: 'oa',
-    description: 'Open all channels <a:tik3:777128127804735518>',
+    description: 'Open all channels',
     execute(message) {
-        const db = require('../db')
-        const json_log = db.data(message.guild.id)
+        const db = require('../db');
+        const json_log = db.data(message.guild.id);
         delete db;
         if (!(json_log.white_list.includes(message.author.id) || message.author.id === message.guild.ownerID)) {
-            return message.channel.send({ embed: { title: "you arent a part of whitelist <a:down:777128205991280640>", color: 0xfc2003 } })
+            return message.channel.send({ embed: { title: "you arent a part of whitelist <a:down:777128205991280640>", color: 0xfc2003 } });
         }
         message.guild.channels.cache.filter(channel => channel.type === "voice" || channel.type === "text").forEach(channel => {
             channel.updateOverwrite(message.guild.roles.everyone, {
                 SEND_MESSAGES: null,
                 VIEW_CHANNEL: null,
             })
-        })
-        return message.channel.send({embed : {title: "completed"}})
+        });
+        return message.channel.send({embed : {title: "completed"}});
     }
-}
-}catch(err){
-    console.log(err)
 }
