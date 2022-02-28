b853b03e73767a99832682c8d5bec7dd188b1fa0
niko_kokawa <96712618+arianhei@users.noreply.github.com>
2022-02-28 08:52:03 +0330

    Update gr.js

--- commands/gr.js
+++ commands/gr.js
@@ -1,7 +1,6 @@
-try{
 module.exports = {
     name: 'gr',
-    description: 'Give role of member or everyone <a:tik3:777128127804735518>',
+    description: 'Give role of member or everyone',
     execute(message) {
         const db = require('../db')
         const json_log = db.data(message.guild.id)
@@ -12,16 +11,16 @@ module.exports = {
         const args = message.content.slice(json_log.PREFIX).trim().split(/ +/);
         delete db;
         if (args.length === 1) {
-            return message.channel.send({ embed: { title: "<a:qal:777127378253774858> Error 0x504: you forgot to mention member", description: `Ex: ${json_log.PREFIX}rg \`@everyone\` \`@role\` ` } })
+            return message.channel.send({ embed: { title: "Error 0x504: you forgot to mention member", description: `Ex: ${json_log.PREFIX}rg \`@everyone\` \`@role\` ` } })
         }
         if (args[1] === "all" || args[1] === "@everyone") {
             const ro = message.mentions.roles
             if (ro === null) {
-                return message.channel.send({ embed: {title: "<a:qal:777127378253774858> ERROR 0x505: you forgot to mention role!", description: `Ex: ${json_log.PREFIX}rg \`@everyone\` \`@role\``} })
+                return message.channel.send({ embed: {title: "ERROR 0x505: you forgot to mention role!", description: `Ex: ${json_log.PREFIX}rg \`@everyone\` \`@role\``} })
             }
             ro.forEach(ro => {
                 if(ro.managed){
-                    return message.channel.send({embed: {title: `Error 0x707: i cant give a ${ro.name} because i detected its for bot <a:qal:777127378253774858>`}})
+                    return message.channel.send({embed: {title: `Error 0x707: i cant give a ${ro.name} because i detected its for bot`}})
                 }
                 if(ro.position < message.guild.me.roles.highest.position)
                 {
@@ -41,7 +40,7 @@ module.exports = {
         else {
             let r = message.mentions.roles;
             if (r.first() === undefined) {
-                return message.channel.send({embed: {title: "<a:qal:777127378253774858> Error 0x505: you forgot to mention role!", description: `Ex: ${json_log.PREFIX}gr \`@everyone\` \`@role\``}})
+                return message.channel.send({embed: {title: "Error 0x505: you forgot to mention role!", description: `Ex: ${json_log.PREFIX}gr \`@everyone\` \`@role\``}})
             }
             let user = message.mentions.users.first()
             if(!user)
@@ -51,33 +50,30 @@ module.exports = {
             let member = message.guild.member(user)
             if(!member)
             {
-                return message.channel.send({embed: {title: "member not found <a:qal:777127378253774858>"}})
+                return message.channel.send({embed: {title: "member not found"}})
             }
             if(member.user.id === message.author.id)
             {
-                return message.channel.send({embed: {title: 'Error 0x204: you can\'t give role to your self <a:qal:777127378253774858>'}})
+                return message.channel.send({embed: {title: 'Error 0x204: you can\'t give role to your self'}})
             }
             if(member.id === message.guild.me.id)
             {
-                return message.channel.send({embed: {title: "i cant give role to myself <a:qal:777127378253774858>"}})
+                return message.channel.send({embed: {title: "i cant give role to myself"}})
             }
             
             r.forEach(ro => {
                 if(ro.managed){
-                    return message.channel.send({embed: {title: `Error 0x707: i cant give a ${ro.name} because i detected its for bot <a:qal:777127378253774858>`}})
+                    return message.channel.send({embed: {title: `Error 0x707: i cant give a ${ro.name} because i detected its for bot`}})
                 }
                 if(ro.position < message.guild.me.roles.highest.position)
                 {
                     member.roles.add(ro)
                 }
                 else{
-                    return message.channel.send({embed: {description: `Error 0x506: i cant give a ${ro.name} because its higher than my role <a:qal:777127378253774858>`}})
+                    return message.channel.send({embed: {description: `Error 0x506: i cant give a ${ro.name} because its higher than my role`}})
                 }
-                message.channel.send({embed: {title: `Gave ${ro.name} Successfully <a:tik4:777128881450647553>`}})
+                message.channel.send({embed: {title: `Gave ${ro.name} Successfully`}})
             });
         }
     }
-}
-}catch(err){
-    console.log(err)
 }
